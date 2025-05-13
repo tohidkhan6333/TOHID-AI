1cc74cfcfbbf3392e908a18a91e357c274ecc2e9
Tohid khan <154800445+tohidkhan6333@users.noreply.github.com>
2025-05-13 17:50:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'TOHID-AI';
 global.pairingNumber = process.env.BOT_NUMBER || '918930358452' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'NvtS2B5Z#mYXdLCKvWEuM-oNgbERRlU7nnpatbuJjiBVe1SFI7SM' ;  // put your session id here
 
 global.mods = []
 global.prems = []
